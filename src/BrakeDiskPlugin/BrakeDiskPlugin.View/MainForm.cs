6ea6f42b9790c00d3c56c5a5bd43e5a618963bfc
ifxrus <danila.khayrulin@gmail.com>
2023-12-10 14:32:49 +0300

    Initial project setup completed

---
+++ src/BrakeDiskPlugin/BrakeDiskPlugin.View/MainForm.cs
@@ -0,0 +1,16 @@
+namespace BrakeDiskPlugin.View
+{
+    /// <summary>
+    /// The main class of the program interface.
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// The main method of the program interface.
+        /// </summary>
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
