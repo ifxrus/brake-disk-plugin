866d1aa22667d05a7367385d268ed894b0ad856a
ifxrus <86190747+ifxrus@users.noreply.github.com>
2023-12-19 00:37:30 +0700

    Program interface (#3)
    * Application interface layout completed
    
    * Added interface response to data changes
    
    * Оставил замечание
    
    * Work on the interface has been completed
    
    ---------
    
    Co-authored-by: vladimir.shvoev <vladimir.shvoev@50ohm.tech>

---
+++ src/BrakeDiskPlugin/BrakeDiskPlugin.Model/Parameters/ParameterType.cs
@@ -0,0 +1,58 @@
+﻿namespace BrakeDiskPlugin.Model.Parameters
+{
+    using System.ComponentModel;
+
+    /// <summary>
+    /// Enumeration listing brake disc parameter types.
+    /// </summary>
+    public enum ParameterType
+    {
+        /// <summary>
+        /// Diameter of the brake disc.
+        /// </summary>
+        [Description("Диаметр тормозного диска.")]
+        BrakeDiskDiameter,
+
+        /// <summary>
+        /// Diameter of the fastener.
+        /// </summary>
+        [Description("Диаметр крепежного элемента.")]
+        FastenerDiameter,
+
+        /// <summary>
+        /// Diameter of the larger fastener.
+        /// </summary>
+        [Description("Диаметр более крупного крепежного элемента.")]
+        LargerFastenerDiameter,
+
+        /// <summary>
+        /// Diameter of the smaller fastener.
+        /// </summary>
+        [Description("Диаметр меньшего крепежного элемента.")]
+        SmallerFastenerDiameter,
+
+        /// <summary>
+        /// Diameter used for centering.
+        /// </summary>
+        [Description("Диаметр, используемый для центрирования.")]
+        CenteringDiameter,
+
+        /// <summary>
+        /// Width of the working surface.
+        /// </summary>
+        [Description("Ширина рабочей поверхности.")]
+        WidthWorkingSurface,
+
+        /// <summary>
+        /// Width of the larger fastener.
+        /// </summary>
+        [Description("Ширина более крупного крепежного элемента.")]
+        WidthLargerFastener,
+
+        /// <summary>
+        /// Width of the smaller fastener.
+        /// </summary>
+        [Description("Ширина меньшего крепежного элемента.")]
+        WidthSmallerFastener
+    }
+}
---
+++ src/BrakeDiskPlugin/BrakeDiskPlugin.Model/Validators/InterfaceValidator.cs
@@ -0,0 +1,22 @@
+﻿namespace BrakeDiskPlugin.Model.Validators
+{
+    /// <summary>
+    /// Class responsible for interface validation.
+    /// </summary>
+    public static class InterfaceValidator
+    {
+        /// <summary>
+        /// Validates whether a KeyPress event is valid based on the input character,
+        /// the current text in the TextBox, and a specified maximum length.
+        /// </summary>
+        /// <param name="keyChar">The character being entered.</param>
+        /// <param name="currentText">The current text in the TextBox.</param>
+        /// <param name="maxLength">The maximum length allowed for the text.</param>
+        /// <returns>True if the KeyPress event is valid; otherwise, false.</returns>
+        public static bool IsValidInput(char keyChar, string currentText, int maxLength)
+        {
+            return (char.IsControl(keyChar) || char.IsDigit(keyChar))
+                   && !(currentText.Length >= maxLength && !char.IsControl(keyChar));
+        }
+    }
+}
